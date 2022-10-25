18fdcb7c1bbd14ced90a851b032196936ea99841
Amanda Ross <amikaross@gmail.com>
2022-10-25 08:10:51 -0600

    Clean up update action

--- app/controllers/admin_adopt_apps_controller.rb
+++ app/controllers/admin_adopt_apps_controller.rb
@@ -5,14 +5,11 @@ class AdminAdoptAppsController < ApplicationController
   end
 
   def update
-    @adopt_app = AdoptApp.find(params[:id])
-    @adopt_app_pets = AdoptAppPet.find_by(pet_id: params[:pet_id], adopt_app_id: params[:id])
+    adopt_app = AdoptApp.find(params[:id])
+    adopt_app_pets = AdoptAppPet.find_by(pet_id: params[:pet_id], adopt_app_id: params[:id])
     if params[:approval_status] != nil
-      @adopt_app_pets.update(:approval_status => params[:approval_status])
+      adopt_app_pets.update(:approval_status => params[:approval_status])
     end
-
-    
-
-    redirect_to "/admin/adopt_apps/#{@adopt_app.id}"
+    redirect_to "/admin/adopt_apps/#{adopt_app.id}"
   end
 end
