8f18ab47b51b92bc35839c69a95ca75cc88f33ed
Amanda Ross <amikaross@gmail.com>
2022-10-26 15:14:49 -0600

    List shelters in reverse alphabetical order on admin/shelters instead of regular

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 RSpec.describe "admin shelters index" do 
   describe "As a visitor" do 
     describe "When I visit the admin shelter index" do 
-      it "displays all the shelters in the system listed in alphabetical order by name" do 
+      it "displays all the shelters in the system listed in reverse alphabetical order by name" do 
         shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
         shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
         shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
         visit "/admin/shelters"
         
-        expect("Aurora shelter").to appear_before("Fancy pets of Colorado")
-        expect("Fancy pets of Colorado").to appear_before("RGV animal shelter")
+        expect("RGV animal shelter").to appear_before("Fancy pets of Colorado")
+        expect("Fancy pets of Colorado").to appear_before("Aurora shelter")
       end
 
       it "has a'Shelter's with Pending Applications' section with the name of shelters with pending applications" do 
